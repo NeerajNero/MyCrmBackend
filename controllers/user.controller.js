9938c1e1ba265d2e1968026dcac676a69799f47f
NeerajNero <neerajnero9066@gmail.com>
2025-03-18 23:35:15 +0530

    updated code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import bcrypt from 'bcrypt'
 
 export const login = async(req,res) => {
     try{
@@ -6,6 +7,15 @@ export const login = async(req,res) => {
         if(!email || !password){
             return res.json({message: "please enter email and password"})
         }
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(404).json({message: "User not found Please signup"})
+        }
+        const comparePassword = await bcrypt.compare(password, user.password)
+        if(!comparePassword){
+            return res.status(400).json("Incorrect password")
+        }
+        res.status(200).json("Logged in successfully")
     }catch(error){
         console.log("error occured while logging in",error.message)
         res.status(500).json({error: error.message})
@@ -22,7 +32,8 @@ export const signup = async(req,res) => {
         if(user){
             return res.status(400).json({message: "user already exists please login"})
         }
-        const newUser = new User({name,email,password})
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({name,email,password: hashedPassword})
         await newUser.save()
         res.status(201).json({message: "user Created"})
     }catch(error){
