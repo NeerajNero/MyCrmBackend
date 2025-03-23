573b043a26b2fd595f25a7a94e76b5a5520fd4f7
NeerajNero <neerajnero9066@gmail.com>
2025-03-23 17:56:06 +0530

    updated code

--- db.connect/db.connect.js
+++ db.connect/db.connect.js
@@ -4,7 +4,7 @@ export const initializeDatabase = async() => {
     try{
         const MONGO_URI = process.env.MONGO_URI
         await mongoose.connect(MONGO_URI)
-        console.log("Connected to DB")
+        console.log("Connected to DB Successfully")
     }catch(error){
         console.log("unable to connect to db")
     }
