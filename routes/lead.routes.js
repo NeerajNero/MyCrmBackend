761b4c5117a395a0d37d37c47d598f7bb514bad9
NeerajNero <neerajnero9066@gmail.com>
2025-03-28 09:31:24 +0530

    updated code

--- routes/lead.routes.js
+++ routes/lead.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createLead, getLeads, updateLead, deleteLead } from "../controllers/lead.controller.js"; 
+import { createLead, getLeads, updateLead, deleteLead, getAllLeads } from "../controllers/lead.controller.js"; 
 import { auth } from "../middlewares/auth.js";
 const router = Router()
 
@@ -7,5 +7,6 @@ router.post('/createLead', auth, createLead )
 router.get('/getLeads', auth, getLeads)
 router.put('/updateLead/:leadId', auth, updateLead)
 router.delete('/deleteLead/:leadId', auth, deleteLead )
+router.get('/getAllLeads', auth, getAllLeads)
 
 export default router
